a024f7aeddbf38b22d273c45e2299faee5ec8d9d
Hackerboi <56054095+danieleremin@users.noreply.github.com>
2025-09-17 10:14:20 -0700

    Potential improvement for SEO

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider, CssBaseline, Box } from '@mui/material';
 import theme from './theme.ts';
 import Navbar from './components/Navbar.tsx';
